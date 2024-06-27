e217f1e63e230bbe0506f573c1adac2de01fc2dd
mfikriam <yapkun17@gmail.com>
2024-06-27 14:09:43 +0800

    Rollback Movie Detail component

--- src/components/search/KeywordSearch.tsx
+++ src/components/search/KeywordSearch.tsx
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   View,
   TextInput,
   FlatList,
   TouchableOpacity,
   StyleSheet,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { StackActions } from "@react-navigation/native";
-import { FontAwesome } from "@expo/vector-icons";
-import MovieItem from "../movies/MovieItem";
-import { API_ACCESS_TOKEN } from "@env";
-import { Movie } from "../../types/app";
+} from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { StackActions } from '@react-navigation/native'
+import { FontAwesome } from '@expo/vector-icons'
+import MovieItem from '../movies/MovieItem'
+import { API_ACCESS_TOKEN } from '@env'
+import { Movie } from '../../types/app'
 
 const KeywordSearch: React.FC = () => {
-  const [query, setQuery] = useState<string>("");
-  const [results, setResults] = useState<Movie[]>([]);
-  const navigation = useNavigation();
+  const [query, setQuery] = useState<string>('')
+  const [results, setResults] = useState<Movie[]>([])
+  const navigation = useNavigation()
 
   const getMovieList = async (query: string): Promise<void> => {
-    const path = `search/movie?query=${query}&page=1`;
-    const url = `https://api.themoviedb.org/3/${path}`;
+    const path = `search/movie?query=${query}&page=1`
+    const url = `https://api.themoviedb.org/3/${path}`
     const options = {
-      method: "GET",
+      method: 'GET',
       headers: {
-        accept: "application/json",
+        accept: 'application/json',
         Authorization: `Bearer ${API_ACCESS_TOKEN}`,
       },
-    };
+    }
 
     try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      setResults(data.results);
+      const response = await fetch(url, options)
+      const data = await response.json()
+      setResults(data.results)
     } catch (error) {
-      console.error("Failed to fetch movies:", error);
+      console.error('Failed to fetch movies:', error)
     }
-  };
+  }
 
   const handleSearch = () => {
     if (query.trim()) {
-      getMovieList(query);
+      getMovieList(query)
     }
-  };
+  }
 
   return (
     <View>
@@ -70,8 +70,8 @@ const KeywordSearch: React.FC = () => {
             style={styles.itemContainer}
             onPress={() => {
               navigation.dispatch(
-                StackActions.push("MovieDetail", { id: item.id })
-              );
+                StackActions.push('Movie Detail', { id: item.id }),
+              )
             }}
           >
             <MovieItem
@@ -87,15 +87,15 @@ const KeywordSearch: React.FC = () => {
         showsVerticalScrollIndicator={false}
       />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   inputContainer: {
-    backgroundColor: "#ebebeb",
+    backgroundColor: '#ebebeb',
     marginTop: 10,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginBottom: 18,
     borderRadius: 30,
     paddingHorizontal: 20,
@@ -110,11 +110,11 @@ const styles = StyleSheet.create({
   },
   itemContainer: {
     margin: 8,
-    alignItems: "center",
+    alignItems: 'center',
   },
   list: {
     paddingBottom: 16,
   },
-});
+})
 
-export default KeywordSearch;
+export default KeywordSearch
